05d4d61237765dcea777850cb8d17c47df952a17
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-05-27 09:50:29 +0300

    setting up my postgress database

--- gallery/settings.py
+++ gallery/settings.py
@@ -66,8 +66,10 @@ WSGI_APPLICATION = 'gallery.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gallery',
+        'USER': 'moringa',
+        'PASSWORD':'cherry',
     }
 }
 
