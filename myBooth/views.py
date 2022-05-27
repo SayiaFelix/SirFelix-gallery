b4269780956da3ba839f519a9fd3faf2119864b4
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-05-27 11:36:24 +0300

    writing view function for retrieving image from database

--- myBooth/views.py
+++ myBooth/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-
+from django.http  import HttpResponse,Http404
+import datetime as dt
+from .models import Image
 # Create your views here.
 def my_gallery(request):
-    
-    return render(request, 'gallery.html')
+
+    date = dt.date.today()
+    Images= Image.objects.all()
+    return render(request, 'gallery.html', {"date": date, "Images": Images})
