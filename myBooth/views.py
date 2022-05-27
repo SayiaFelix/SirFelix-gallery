e2ebf46f191d91c18470c7ad8d8b861fba5dfe41
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-05-27 12:13:54 +0300

    writing view function for gallery

--- myBooth/views.py
+++ myBooth/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http  import HttpResponse,Http404
 import datetime as dt
-from .models import Image
+from .models import Image, category,Location
+
 # Create your views here.
 def my_gallery(request):
 
+    Images = Image.show_all_photos()
+    locations = Location.objects.all()
     date = dt.date.today()
-    Images= Image.objects.all()
-    return render(request, 'gallery.html', {"date": date, "Images": Images})
+    # Images= Image.objects.all()
+    return render(request, 'gallery.html', {"date": date, "Images": Images,"locations":locations})
+
+
+
