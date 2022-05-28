4e09cc18059cae2da32309954d620dd03a17de83
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-05-28 09:11:17 +0300

    fixing error on url

--- myBooth/urls.py
+++ myBooth/urls.py
@@ -3,8 +3,9 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [ 
-    re_path('', views.my_gallery, name='my_gallery'),
-    re_path(r'search/', views.search_results, name='search_results'),
+    re_path('^$', views.my_gallery, name='my_gallery'),
+    # re_path(r'search/', views.search_results, name='search_results'),
+    re_path(r'^search/', views.search_results, name='search_results'),
     re_path(r'category/(\d+)', views.get_category, name = 'get_category'),
     re_path(r'location/(\d+)', views.get_location, name = 'get_location'),
   
--- myBooth/views.py
+++ myBooth/views.py
@@ -12,37 +12,27 @@ def my_gallery(request):
     # Images= Image.objects.all()
     return render(request, 'all-images/gallery.html', {"date": date, "Images": Images,"locations":locations})
 
-
 def search_results(request):
-    if 'image' in request.GET and request.GET["image"]:
-        # category = request.GET.get("search")
-        # searched_photos = Image.search_photo_by_category(category)
+    if 'search' in request.GET and request.GET["search"]:
+        category = request.GET.get("search")
+        searched_photos = Image.search_photo_by_category(category)
         locations = Location.objects.all()
-        # message = f"{category}"
+        message = f"{category}"
 
-        search_term = request.GET.get("image")
-        searched_images = Image.search_by_title(search_term)
-        message = f"{search_term}"
-          
-        return render(request, 'search.html', {"message":message, "images":searched_images, "locations":locations })
+        return render(request, 'all-images/search-images.html', {"message":message, "Images":searched_photos, "locations":locations})
 
     else:
-        message = "You haven't searched for anything!!"
-        return render(request, 'all-images/search.html',{"message":message})
-
-
-
-
+        message = "You haven't searched for any term"
+        return render(request, 'all-images/search-images.html', {"message":message})
 
 def get_category(request, category):
     category_results = category.objects.all()
     location_results = Location.objects.all()
-    category_result = Image.objects.filter(category_name__category = category)
-    return render(request,'gallery.html',{'all_images':category_result,'category_results':category_results,'location_results':location_results})
+    category_result = Image.objects.filter(category__category_name = category)
+    return render(request,'all-images/gallery.html',{'all_images':category_result,'category_results':category_results,'location_results':location_results})
 
-def get_location(request, location):
+def get_location(request, location_name):
     category_results = category.objects.all()
     locations = Location.objects.all()
-    location_result = Image.objects.filter(location_name__id= location)
-    return render(request,'locations.html',{'all_images':location_result,'category_results':category_results,'locations':locations})
-
+    location_result = Image.objects.filter(location__id= location_name)
+    return render(request,'all-images/locations.html',{'all_images':location_result,'category_results':category_results,'locations':locations})
