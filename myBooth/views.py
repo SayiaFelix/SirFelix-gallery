36672c7266073a7adf9bdabcb8e4decf70a4d351
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-05-28 11:46:10 +0300

    editing location file

--- myBooth/views.py
+++ myBooth/views.py
@@ -6,25 +6,10 @@ from .models import Image, category,Location
 
 # Create your views here.
 def my_gallery(request):
-
     Images = Image.show_all_photos()
     locations = Location.objects.all()
     date = dt.date.today()
-    # Images= Image.objects.all()
     return render(request, 'all-images/gallery.html', {"date": date, "Images": Images,"locations":locations})
-
-# def search_results(request):
-#     if 'search' in request.GET and request.GET["search"]:
-#         category = request.GET.get("search")
-#         searched_photos = Image.search_photo_by_category(category)
-#         locations = Location.objects.all()
-#         message = f"{category}"
-
-#         return render(request, 'all-images/search-images.html', {"message":message, "Images":searched_photos, "locations":locations})
-
-#     else:
-#         message = "You haven't searched for any term"
-#         return render(request, 'all-images/search-images.html', {"message":message})
 
 def search_results(request):
 
@@ -40,22 +25,17 @@ def search_results(request):
         return render(request, 'all-images/search-images.html',{"message":message})
 
 
-def get_category(request, category):
-    category_results = category.objects.all()
-    location_results = Location.objects.all()
-    category_result = Image.objects.filter(category__category_name = category)
-    return render(request,'all-images/gallery.html',{'all_images':category_result,'category_results':category_results,'location_results':location_results})
-
-# def get_location(request, location_name):
-
-#     category_results = category.objects.all()
-#     locations = Location.objects.all()
-#     location_result = Image.objects.filter(location__id= location_name)
-#     return render(request,'all-images/locations.html',{'Images':location_result,'category_results':category_results,'locations':locations})
-
 def get_location(request, location_id ):
     try:
         image = Image.objects.get(id= location_id )
     except DoesNotExist:
         raise Http404()
     return render(request,"all-images/locations.html", {"image":image})
+
+
+def get_category(request, category):
+    category_results = category.objects.all()
+    location_results = Location.objects.all()
+    category_result = Image.objects.filter(category__category_name = category)
+    return render(request,'all-images/gallery.html',{'Images':category_result,'category_results':category_results,'location_results':location_results})
+
