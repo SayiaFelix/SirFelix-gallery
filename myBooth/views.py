189127b4a7446c566fa9c5670c699c2ba0840206
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-05-29 10:18:09 +0300

    fixing bugs

--- myBooth/views.py
+++ myBooth/views.py
@@ -1,15 +1,14 @@
-
 from django.shortcuts import render,redirect
 from django.http  import Http404
 import datetime as dt
-from .models import Image,Location, category
+from .models import Image,Location
 
 # Create your views here.
 def my_gallery(request):
-    Images = Image.show_all_photos()
+    Images = Image.show_all_photos() # Show all photos in oder of date posted
     locations = Location.objects.all()
     date = dt.date.today()
-    # Images = Image.objects.all()
+    Images = Image.objects.all()
     return render(request, 'all-images/gallery.html', {"date": date, "Images": Images,"locations":locations})
 
 def search_results(request):
@@ -31,12 +30,6 @@ def get_location(request, location_id ):
     except DoesNotExist:
         raise Http404()
     return render(request,"all-images/locations.html", {"image":image})
-
-# def get_location(request, location_name):
-#     category_results = category.objects.all()
-#     locations = Location.objects.all()
-#     location_result = Image.objects.filter(location__id= location_name)
-#     return render(request,'all-images/locations.html',{'all_images':location_result,'category_results':category_results,'locations':locations})
 
 def delete_image(request, image_id):
     image=Image.objects.filter(id = image_id).delete()
