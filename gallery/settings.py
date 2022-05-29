083fb5b575ae0022cd42a46d4a9067d9ff6ca369
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-05-29 13:54:01 +0300

    setting files for deployment

--- gallery/settings.py
+++ gallery/settings.py
@@ -124,7 +124,7 @@ STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
+#
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
